66332cad25e96540d1a8d20bacd31f64f9fa4043
eren23 <erenak1996@gmail.com>
2021-01-22 21:05:01 +0100

    Day 27

--- day26/simple-load-balancer/main.go
+++ day26/simple-load-balancer/main.go
@@ -71,3 +71,39 @@ func (b *Backend) IsAlive() (alive bool) {
 	return
   }
   
+  // lb load balances the incoming request
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := serverPool.GetNextPeer()
+	if peer != nil {
+	  peer.ReverseProxy.ServeHTTP(w, r)
+	  return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+  }
+
+  server := http.Server{
+	Addr:    fmt.Sprintf(":%d", port),
+	Handler: http.HandlerFunc(lb),
+  }
+  
+proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, e error) {
+  log.Printf("[%s] %s\n", serverUrl.Host, e.Error())
+  retries := GetRetryFromContext(request)
+  if retries < 3 {
+    select {
+      case <-time.After(10 * time.Millisecond):
+        ctx := context.WithValue(request.Context(), Retry, retries+1)
+        proxy.ServeHTTP(writer, request.WithContext(ctx))
+      }
+      return
+    }
+
+  // after 3 retries, mark this backend as down
+  serverPool.MarkBackendStatus(serverUrl, false)
+
+  // if the same request routing for few attempts with different backends, increase the count
+  attempts := GetAttemptsFromContext(request)
+  log.Printf("%s(%s) Attempting retry %d\n", request.RemoteAddr, request.URL.Path, attempts)
+  ctx := context.WithValue(request.Context(), Attempts, attempts+1)
+  lb(writer, request.WithContext(ctx))
+}
---
+++ day27/README.md
@@ -0,0 +1,3 @@
+# Day27
+
+Kept working on load balancer, not yet finished
